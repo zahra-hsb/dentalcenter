d76d75dae93fe9025862d1bc9f5af34cb71d3473
zahra <zahra20hb@gmail.com>
2024-09-15 14:27:48 -0700

    responsive header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,14 +4,14 @@ import Button from "../globalComponents/Button"
 const Header = () => {
     return (
         <>
-            <header className="py-10 px-20 flex justify-between">
+            <header className="py-5 sm:py-10 px-5 sm:px-20 flex justify-between items-center bg-opacity-50 m-5 sm:m-0 bg-white sm:bg-bgColor shadow">
                 <nav className="">
                     <Button link={'tel:#'}>
                         تماس
                     </Button>
                 </nav>
                 <nav>
-                    <ul className="flex justify-between items-center gap-10">
+                    <ul className="hidden sm:flex justify-between items-center gap-10">
                         <li>
                             <Link href={'#'}>
                                 درباره ما
@@ -32,7 +32,7 @@ const Header = () => {
                     </ul>
                 </nav>
                 <nav>
-                    <h3 className="uppercase text-darkGreen font-extrabold text-3xl">
+                    <h3 className="uppercase text-darkGreen font-extrabold text-2xl sm:text-3xl">
                         dental
                         <span className="text-green">center</span>
                     </h3>
--- src/components/globalComponents/Button/index.jsx
+++ src/components/globalComponents/Button/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Button = ({ link, children, style }) => {
     return (
         <>
-            <Link href={link} className={'text-green bg-gradient-to-r from-darkGreen to-buttonOp px-8 font-bold py-3 rounded-lg ' + style} >{children}</Link>
+            <Link href={link} className={'text-green bg-gradient-to-r from-darkGreen to-buttonOp px-5 sm:px-8 font-bold py-2 sm:py-3 rounded-lg ' + style} >{children}</Link>
         </>
     )
 }
