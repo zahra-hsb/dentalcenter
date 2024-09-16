7756356c143e3a6089c1f4c48201978c53aa52e9
zahra <zahra20hb@gmail.com>
2024-09-16 12:31:58 -0700

    desktop menu, logo and phone button components created and react icons installed and an icon added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,42 +1,20 @@
 import Link from "next/link"
 import Button from "../globalComponents/Button"
+import DesktopMenu from "../globalComponents/DesktopMenu"
+import Logo from "../globalComponents/Logo"
+import { IoMenu } from "react-icons/io5";
+import PhoneButton from "../globalComponents/PhoneButton";
 
 const Header = () => {
     return (
         <>
             <header className="py-5 sm:py-10 px-3 sm:px-16 lg:px-16 flex justify-between items-center gap-5 bg-opacity-50 sm:bg-opacity-100 m-5 sm:m-0 bg-white sm:bg-bgColor shadow sm:shadow-none">
-                <nav className="">
-                    <Button link={'tel:#'}>
-                        تماس
-                    </Button>
-                </nav>
-                <nav>
-                    <ul className="hidden sm:flex justify-between items-center gap-10">
-                        <li>
-                            <Link href={'#'}>
-                                درباره ما
-                            </Link>
-                        </li>
-                        {/* <li></li> */}
-                        <li>
-                            <Link href={'#'}>
-                                تماس با ما
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={'/'} >
-                                صفحه اصلی
-                            </Link>
-                        </li>
-
-                    </ul>
-                </nav>
-                <nav className="">
-                    <h3 className="uppercase text-darkGreen font-extrabold text-lg sm:text-3xl">
-                        dental
-                        <span className="text-green">center</span>
-                    </h3>
-                </nav>
+                <PhoneButton />
+                <span>
+                    <IoMenu size={25} />
+                </span>
+                <DesktopMenu />
+                <Logo />
             </header>
         </>
     )
