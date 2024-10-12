93377c22c8eb0cb44d5ea88ca1360be0fe228ac2
zahra-hsb <zahra16toa@gmail.com>
2024-10-12 14:14:08 -0700

    several sections added to footer section

--- src/components/globalComponents/Footer/index.jsx
+++ src/components/globalComponents/Footer/index.jsx
@@ -1,12 +1,48 @@
 import Menu from "../Menu"
+import phone from '../../../../public/icons/phone.svg'
+import insta from '../../../../public/icons/insta.svg'
+import Logo from "../Logo"
+import Link from "next/link"
+import Image from "next/image"
+import IconContainer from "../IconContainer"
+import { FaTelegramPlane } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <>
-            <footer className="bg-green w-full lg:px-32 py-10 h-full bg-opacity-20">
+            <footer className="bg-green w-full lg:px-32 py-10 h-full bg-opacity-20 flex justify-between items-center">
                 <div className="flex flex-col items-start gap-5">
                     <h4 className="text-lg font-semibold">فهرست</h4>
                     <Menu style={'flex-col !items-start text-gray-600 text-sm !gap-5'} />
+                </div>
+                <div className="flex flex-col items-start gap-5">
+                    <h4 className="text-lg font-semibold">تماس با ما</h4>
+                    <div>
+                        <p className="text-sm mb-1">شماره تلفن:</p>
+                        <p className="text-gray-500 text-sm">0214807225 - 09307719208</p>
+                    </div>
+                    <div>
+                        <p className=" text-sm mb-1">آدرس:</p>
+                        <p className="text-gray-500 text-sm">شهرقدس، میدان قدس، خیابان امام خمینی، جنب مسجد جامع</p>
+                    </div>
+                </div>
+                <div className="flex flex-col items-start gap-5">
+                    <Logo />
+                    <h5 className="text- text-md lg:text-xl font-bold my-1">زیبایی را با ما تجربه کنید.</h5>
+                    <div className="flex justify-between items-center gap-5">
+                        <Link href={'#'}>
+                            <IconContainer style={'!bg-white hover:!bg-darkGreen transition-all duration-300'}>
+                                <Image src={insta} alt="" />
+                            </IconContainer>
+                        </Link>
+                        <Link href={'#'}>
+                            <IconContainer style={'!bg-white hover:!bg-darkGreen transition-all duration-300'}>
+                                <FaTelegramPlane className="text-green" size={25} />
+                            </IconContainer>
+                        </Link>
+
+                    </div>
+
                 </div>
             </footer>
         </>
--- src/components/globalComponents/IconContainer/index.jsx
+++ src/components/globalComponents/IconContainer/index.jsx
@@ -1,7 +1,7 @@
-const IconContainer = ({ children }) => {
+const IconContainer = ({ children, style }) => {
     return (
 
-            <span className="w-12 h-12 p-3 rounded-lg bg-darkGreen flex items-center justify-center">
+            <span className={`w-12 h-12 p-3 rounded-lg bg-darkGreen flex items-center justify-center ${style}`}>
                 {children}
             </span>
 
