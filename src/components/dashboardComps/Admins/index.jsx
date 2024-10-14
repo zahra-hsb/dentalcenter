cdd2e77a76254b9f2193d47fc43ad82a77012266
zahra-hsb <zahra16toa@gmail.com>
2024-10-14 08:52:41 -0700

    admin row compoenent created

--- src/components/dashboardComps/Admins/index.jsx
+++ src/components/dashboardComps/Admins/index.jsx
@@ -1,12 +1,17 @@
 'use client'
 import Button from "@/components/globalComponents/Button";
 import Link from "next/link"
-import { BiShowAlt } from "react-icons/bi";
-import { GrEdit } from "react-icons/gr";
-import { RiDeleteBinLine } from "react-icons/ri";
+
 import { IoPersonAddOutline } from "react-icons/io5";
 import { useState } from "react";
 import Modal from "@/components/globalComponents/Modal";
+import AdminRow from "./AdminRow";
+
+const adminsArray = [
+    { id: 0, name: 'دکتر وحید گماریان', username: 'VGomaryan@', tel: '09914454546', image: '' },
+
+]
+
 
 const Admins = () => {
     const [areDelete, setDelete] = useState(false)
@@ -25,7 +30,7 @@ const Admins = () => {
         setShowModal(false)
     }
 
-    
+
     return (
         <>
             <section className="p-20">
@@ -75,48 +80,15 @@ const Admins = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr className="hover:bg-gray-100 cursor-pointer transition-all duration-300">
-                                    <td className="p-4 border-b border-blue-gray-50">
-                                        <div class="ml-5">
-                                            <div class="bg-gray-200 rounded-sm w-5 h-5 flex flex-shrink-0 justify-center items-center relative">
-                                                <input placeholder="checkbox" type="checkbox" class="checkbox absolute cursor-pointer w-full h-full" />
-                                                <div class="check-icon hidden bg-indigo-700 text-white rounded-sm">
-                                                    <svg class="icon icon-tabler icon-tabler-check" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z"></path>
-                                                        <path d="M5 12l5 5l10 -10"></path>
-                                                    </svg>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
+                                {adminsArray.length > 0 ? adminsArray.map(item => (
+                                    <AdminRow name={item.name} username={item.username} tel={item.tel} key={item.id} />
+                                ))
+                                    :
+                                    <div>
+                                        موردی برای نمایش وجود ندارد...
+                                    </div>
+                                }
 
-                                    <td class="p-4 border-b border-blue-gray-50">
-                                        <div class="flex items-center justify-center">
-                                            <img src="https://docs.material-tailwind.com/img/logos/logo-spotify.svg" alt="Spotify" class="inline-block relative object-center !rounded-full w-12 h-12 rounded-lg border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1" />
-                                        </div>
-                                    </td>
-                                    <td class="p-4 border-b border-blue-gray-50 w-full lg:w-auto">
-                                        <div class="">
-                                            <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold hover:underline cursor-pointer hover:text-green transition-all duration-300">دکتر وحید گماریان</p>
-                                        </div>
-                                    </td>
-                                    <td class="p-4 border-b border-blue-gray-50">
-                                        <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal text-center">09999999999</p>
-                                    </td>
-                                    <td class="p-4 border-b border-blue-gray-50">
-                                        <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal ">VGomaryan@</p>
-                                    </td>
-                                    <td class="p-4 border-b border-blue-gray-50">
-                                        <Link href={'/dashboard/blog/edit'} class="relative flex justify-center items-center select-none w-full hover:text-lime-500  transition-all duration-300" >
-                                            <GrEdit size={20} />
-                                        </Link>
-                                    </td>
-                                    <td class="p-4 border-b border-blue-gray-50">
-                                        <Link href={'/dashboard/blog?delete'} class="relative flex justify-center items-center select-none w-full hover:text-red-500  transition-all duration-300" >
-                                            <RiDeleteBinLine size={20} />
-                                        </Link>
-                                    </td>
-                                </tr>
 
                             </tbody>
                         </table>
