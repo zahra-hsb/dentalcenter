a3a87d558fcfad4b3ce9354b86d41a657f865eea
zahra-hsb <zahra16toa@gmail.com>
2024-10-14 08:24:00 -0700

    blog list created with map

---
+++ src/components/dashboardComps/DashboardBlogList/BlogRow/index.jsx
@@ -0,0 +1,60 @@
+import Link from "next/link"
+import { BiShowAlt } from "react-icons/bi"
+import { GrEdit } from "react-icons/gr"
+import { RiDeleteBinLine } from "react-icons/ri"
+
+const BlogRow = ({ title, image, author, date }) => {
+    return (
+        <>
+            <tr className="hover:bg-gray-100 cursor-pointer transition-all duration-300">
+                <td className="p-4 border-b border-blue-gray-50">
+                    <div class="ml-5">
+                        <div class="bg-gray-200 rounded-sm w-5 h-5 flex flex-shrink-0 justify-center items-center relative">
+                            <input placeholder="checkbox" type="checkbox" class="checkbox absolute cursor-pointer w-full h-full" />
+                            <div class="check-icon hidden bg-indigo-700 text-white rounded-sm">
+                                <svg class="icon icon-tabler icon-tabler-check" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z"></path>
+                                    <path d="M5 12l5 5l10 -10"></path>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+
+                <td class="p-4 border-b border-blue-gray-50">
+                    <div class="flex items-center justify-center">
+                        <img src="https://docs.material-tailwind.com/img/logos/logo-spotify.svg" alt="Spotify" class="inline-block relative object-center !rounded-full w-12 h-12 rounded-lg border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1" />
+                    </div>
+                </td>
+                <td class="p-4 border-b border-blue-gray-50 w-full lg:w-auto">
+                    <div class="">
+                        <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold hover:underline cursor-pointer hover:text-green transition-all duration-300">{title != '' && title}</p>
+                    </div>
+                </td>
+                <td class="p-4 border-b border-blue-gray-50">
+                    <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">{author != '' && author}</p>
+                </td>
+                <td class="p-4 border-b border-blue-gray-50">
+                    <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">{date != '' && date}</p>
+                </td>
+                <td class="p-4 border-b border-blue-gray-50">
+                    <Link href={'#'} class="relative flex justify-center items-center select-none w-full hover:text-green transition-all duration-300" >
+                        <BiShowAlt size={25} />
+                    </Link>
+                </td>
+                <td class="p-4 border-b border-blue-gray-50">
+                    <Link href={'/dashboard/blog/edit'} class="relative flex justify-center items-center select-none w-full hover:text-lime-500  transition-all duration-300" >
+                        <GrEdit size={20} />
+                    </Link>
+                </td>
+                <td class="p-4 border-b border-blue-gray-50">
+                    <Link href={'/dashboard/blog?delete'} class="relative flex justify-center items-center select-none w-full hover:text-red-500  transition-all duration-300" >
+                        <RiDeleteBinLine size={20} />
+                    </Link>
+                </td>
+            </tr>
+        </>
+    )
+}
+
+export default BlogRow
--- src/components/dashboardComps/DashboardBlogList/index.jsx
+++ src/components/dashboardComps/DashboardBlogList/index.jsx
@@ -1,9 +1,13 @@
 import Link from "next/link"
-import { BiShowAlt } from "react-icons/bi";
-import { GrEdit } from "react-icons/gr";
-import { IoPersonAddOutline } from "react-icons/io5";
-import { RiDeleteBinLine } from "react-icons/ri";
 import { FaRegEdit } from "react-icons/fa";
+import BlogRow from "./BlogRow";
+
+const blogsArray = [
+    { id: 0, title: 'کامپوزیت چیست؟', author: 'دکتر وحید گماریان', dateOfBlog: '1403/5/3', image: '' },
+    { id: 1, title: 'تفاوت بین لمینت و کامپوزیت', author: 'دکتر وحید گماریان', dateOfBlog: '1403/5/20', image: '' },
+    { id: 2, title: 'راز دندان های سفید و سالم', author: 'دکتر وحید گماریان', dateOfBlog: '1403/6/10', image: '' },
+    // { id: 3, title: '', author: '', dateOfBlog: '', image: '' },
+]
 
 const DashboardBlogList = () => {
     return (
@@ -56,53 +60,14 @@ const DashboardBlogList = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="hover:bg-gray-100 cursor-pointer transition-all duration-300">
-                            <td className="p-4 border-b border-blue-gray-50">
-                                <div class="ml-5">
-                                    <div class="bg-gray-200 rounded-sm w-5 h-5 flex flex-shrink-0 justify-center items-center relative">
-                                        <input placeholder="checkbox" type="checkbox" class="checkbox absolute cursor-pointer w-full h-full" />
-                                        <div class="check-icon hidden bg-indigo-700 text-white rounded-sm">
-                                            <svg class="icon icon-tabler icon-tabler-check" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z"></path>
-                                                <path d="M5 12l5 5l10 -10"></path>
-                                            </svg>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-
-                            <td class="p-4 border-b border-blue-gray-50">
-                                <div class="flex items-center justify-center">
-                                    <img src="https://docs.material-tailwind.com/img/logos/logo-spotify.svg" alt="Spotify" class="inline-block relative object-center !rounded-full w-12 h-12 rounded-lg border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1" />
-                                </div>
-                            </td>
-                            <td class="p-4 border-b border-blue-gray-50 w-full lg:w-auto">
-                                <div class="">
-                                    <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold hover:underline cursor-pointer hover:text-green transition-all duration-300">کامپوزیت چیست؟</p>
-                                </div>
-                            </td>
-                            <td class="p-4 border-b border-blue-gray-50">
-                                <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">دکتر وحید گماریان</p>
-                            </td>
-                            <td class="p-4 border-b border-blue-gray-50">
-                                <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">1403/2/15</p>
-                            </td>
-                            <td class="p-4 border-b border-blue-gray-50">
-                                <Link href={'#'} class="relative flex justify-center items-center select-none w-full hover:text-green transition-all duration-300" >
-                                    <BiShowAlt size={25} />
-                                </Link>
-                            </td>
-                            <td class="p-4 border-b border-blue-gray-50">
-                                <Link href={'/dashboard/blog/edit'} class="relative flex justify-center items-center select-none w-full hover:text-lime-500  transition-all duration-300" >
-                                    <GrEdit size={20} />
-                                </Link>
-                            </td>
-                            <td class="p-4 border-b border-blue-gray-50">
-                                <Link href={'/dashboard/blog?delete'} class="relative flex justify-center items-center select-none w-full hover:text-red-500  transition-all duration-300" >
-                                    <RiDeleteBinLine size={20} />
-                                </Link>
-                            </td>
-                        </tr>
+                        {blogsArray.length > 0 ? blogsArray.map(item =>
+                            <BlogRow author={item.author} date={item.dateOfBlog} key={item.id} title={item.title} />
+                        )
+                            :
+                            <div>
+                                موردی برای نمایش وجود ندارد...
+                            </div>
+                        }
 
                     </tbody>
                 </table>
