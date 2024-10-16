1aa9e19b87758a4945ee2863a191d420bc39b6ce
zahra-hsb <zahra16toa@gmail.com>
2024-10-16 12:11:31 -0700

    add blog page created

--- src/components/dashboardComps/AddBlog/index.jsx
+++ src/components/dashboardComps/AddBlog/index.jsx
@@ -4,6 +4,8 @@ import Input from "@/components/globalComponents/Input"
 import CustomEditor from "../CustomEditor"
 import { useState } from "react"
 import Tag from "@/components/globalComponents/Tag"
+import Button from "@/components/globalComponents/Button"
+import { LuSaveAll } from "react-icons/lu";
 
 const AddBlog = () => {
     const [tag, setTag] = useState('')
@@ -27,26 +29,42 @@ const AddBlog = () => {
         <>
             <section className="p-10 sm:p-20 flex flex-col gap-5">
                 <h4 className="text-xl font-bold">افزودن وبلاگ</h4>
-                <div className="border p-5 rounded-lg w-full bg-white shadow flex flex-col gap-5">
-                    <div className="flex flex-col sm:flex-row items-center justify-between gap-10">
-                        <Input placeholder={'عنوان مقاله'} type={'text'} />
-                        <div className="flex flex-col gap-5 w-full ">
-                            <Input onChange={(e) => handleChangeTag(e)} value={tag} placeholder={'برچسب ها'} type={'text'} onKeyDown={(e) => handleKeyDown(e)} style={''} />
-                            <div className="flex gap-5 w-full flex-wrap">
-                                {tags?.length > 0
-                                    &&
-                                    tags?.map(item => (
-                                        <>
-                                            <Tag tag={item} />
-                                        </>
-                                    ))
-                                }
-                            </div>
-                        </div>
-                    </div>
-                    <h3>متن وبلاگ</h3>
-                    {/* <CustomEditor /> */}
+                <div className="border p-5 rounded-lg w-full bg-white shadow ">
+                    <form className="flex flex-col gap-5">
+                        <div className="flex flex-col sm:flex-row items-center justify-between gap-10">
+                            <Input placeholder={'عنوان مقاله'} type={'text'} style={'w-1/2'} />
 
+                        </div>
+                        <h3>متن وبلاگ</h3>
+                        <CustomEditor />
+                        <div className="flex flex-col sm:flex-row gap-5 items-start justify-between w-full h-full">
+                            <div className="w-full flex flex-col gap-5">
+                                <h3>تصویر مقاله</h3>
+                                <Input type={'file'} />
+                            </div>
+                            <div className="flex flex-col gap-5 w-full ">
+                                <h3>برچسب ها</h3>
+                                <Input onChange={(e) => handleChangeTag(e)} value={tag} placeholder={'برچسب ها'} type={'text'} onKeyDown={(e) => handleKeyDown(e)} style={''} />
+                                <div className="flex gap-5 w-full flex-wrap">
+                                    {tags?.length > 0
+                                        &&
+                                        tags?.map(item => (
+                                            <>
+                                                <Tag tag={item} />
+                                            </>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="flex items-end">
+                            <Button link={'/dashboard/blog'} style={'py-4'}>
+                                <LuSaveAll className="ml-2" size={20} />
+                                افزودن وبلاگ
+                            </Button>
+                        </div>
+                    </form>
                 </div>
             </section>
         </>
