805d81617be26cc83564fb60829b50987d4f6c28
zahra-hsb <zahra16toa@gmail.com>
2024-10-28 05:09:45 -0700

    main page of dashboard has responsived

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -5,16 +5,16 @@ import { PiArticleNyTimesFill } from "react-icons/pi";
 const dashboard = () => {
     return (
         <>
-            <section className="p-20">
+            <section className="p-5 sm:p-20">
                 {/* <!-- component --> */}
-                <h4 className="text-2xl font-semibold">داشبورد مدیریت</h4>
-                <div className="py-10 flex gap-5">
-                    <Container style={'!w-1/3'}>
+                <h4 className="text-2xl font-semibold text-center sm:text-start">داشبورد مدیریت</h4>
+                <div className="py-10 flex flex-col sm:flex-row gap-5">
+                    <Container style={'!w-full sm:!w-1/3'}>
                         {/* <PiArticleNyTimesFill className="group-hover:text-white text-green" size={30} /> */}
                         <span className="text-green group-hover:text-white font-semibold text-3xl">27</span>
                         <h3 className="group-hover:text-green">تعداد مقاله ها</h3>
                     </Container>
-                    <Container style={'!w-1/3'}>
+                    <Container style={'!w-full sm:!w-1/3'}>
                         {/* <PiArticleNyTimesFill className="group-hover:text-white text-green" size={30} /> */}
                         <span className="text-green group-hover:text-white font-semibold text-3xl">2</span>
                         <h3 className="group-hover:text-green">تعداد ادمین ها</h3>
