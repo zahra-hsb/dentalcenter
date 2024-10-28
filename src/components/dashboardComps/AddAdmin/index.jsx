25fe95c08a4dce0692ad119cb763a85c46a227bc
zahra-hsb <zahra16toa@gmail.com>
2024-10-28 11:47:37 -0700

    one problem fixed

--- src/components/dashboardComps/AddAdmin/index.jsx
+++ src/components/dashboardComps/AddAdmin/index.jsx
@@ -7,7 +7,7 @@ const AddAdmin = () => {
     return (
         <>
             <section className="p-10 sm:p-20 flex flex-col gap-5">
-                <h4 className="text-xl font-bold">افزودن وبلاگ</h4>
+                <h4 className="text-xl font-bold">افزودن ادمین</h4>
                 <div className="border p-5 rounded-lg w-full bg-white shadow ">
                     <AdminForm />
                 </div>
--- src/components/dashboardComps/AddBlog/index.jsx
+++ src/components/dashboardComps/AddBlog/index.jsx
@@ -7,7 +7,7 @@ const AddBlog = () => {
     return (
         <>
             <section className="p-10 sm:p-20 flex flex-col gap-5">
-                <h4 className="text-xl font-bold">افزودن ادمین</h4>
+                <h4 className="text-xl font-bold">افزودن وبلاگ</h4>
                 <div className="border p-5 rounded-lg w-full bg-white shadow ">
                     <BlogForm />
                 </div>
