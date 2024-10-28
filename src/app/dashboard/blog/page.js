8a7007fd0f436addb68738e586533a473b582a85
zahra-hsb <zahra16toa@gmail.com>
2024-10-28 05:57:23 -0700

    blog list title added

--- src/app/dashboard/blog/page.js
+++ src/app/dashboard/blog/page.js
@@ -2,7 +2,8 @@ import DashboardBlogList from "@/components/dashboardComps/DashboardBlogList"
 
 const blog = () => {
     return (
-        <section className="p-20">
+        <section className="px-5 py-10 sm:p-20">
+            <h4 className="text-2xl font-semibold text-center sm:text-start mb-10 text-slate-600">لیست بلاگ ها</h4>
             <DashboardBlogList />
         </section>
     )
