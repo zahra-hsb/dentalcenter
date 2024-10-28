37778c0ed1b23b7156ddcb0552b0087c61260df7
zahra-hsb <zahra16toa@gmail.com>
2024-10-28 13:18:34 -0700

    titles section has responsived

--- src/components/homeComponents/TitlesSection/index.jsx
+++ src/components/homeComponents/TitlesSection/index.jsx
@@ -7,17 +7,17 @@ import { FaUserDoctor } from "react-icons/fa6";
 const TitlesSection = () => {
     return (
         <>
-            <section className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10 px-16 pb-20">
+            <section className="grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-10 px-16 pb-20">
                 <Container>
-                    <div className=" text-center h-28 flex flex-col gap-3 items-center justify-center">
+                    <div className=" text-center h-28 flex flex-col gap-3 items-center lg:justify-start justify-center">
                         <GiToothbrush size={40} className="group-hover:text-white text-green" />
-                        <p className="group-hover:text-green">
+                        <p className="group-hover:text-green sm:text-xs lg:text-sm">
                             آموزش مراقبت‌های پیشگیرانه بیماری‌های لثه و دندان
                         </p>
                     </div>
                 </Container>
                 <Container>
-                    <div className=" text-center h-28 flex flex-col gap-3 items-center justify-center">
+                    <div className=" text-center h-28 flex flex-col gap-3 items-center lg:justify-start justify-center ">
                         <TbDentalBroken size={40} className="group-hover:text-white text-green" />
                         <p className="group-hover:text-green">
                             کنترل پلاک و جرم
@@ -25,7 +25,7 @@ const TitlesSection = () => {
                     </div>
                 </Container>
                 <Container>
-                    <div className="text-center h-28 flex flex-col gap-3 items-center justify-center">
+                    <div className="text-center h-28 flex flex-col gap-3 items-center lg:justify-start justify-center ">
                         <TbDental size={40} className="group-hover:text-white text-green" />
                         <p className="group-hover:text-green">
                             ارائه بهترین روش‌های معاینه و تشخیص
@@ -33,10 +33,18 @@ const TitlesSection = () => {
                     </div>
                 </Container>
                 <Container>
-                    <div className="text-center h-28 flex flex-col gap-3 items-center justify-center">
+                    <div className="text-center h-28 flex flex-col gap-3 items-center lg:justify-start justify-center ">
+                        <FaUserDoctor size={40} className="group-hover:text-white text-green" />
+                        <p className="group-hover:text-green sm:text-sm">
+                            درمان بیماران با نهایت احترام و توجه به حساسیت‌ها
+                        </p>
+                    </div>
+                </Container>
+                <Container>
+                    <div className="text-center h-28 flex flex-col gap-3 items-center lg:justify-start justify-center ">
                         <FaUserDoctor size={40} className="group-hover:text-white text-green" />
                         <p className="group-hover:text-green">
-                            درمان بیماران با نهایت احترام و توجه به حساسیت‌ها
+                            ویزیت و مشاوره رایگان
                         </p>
                     </div>
                 </Container>
