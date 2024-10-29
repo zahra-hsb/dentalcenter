bf7451636bb926fefb6f596d46f13a9b82560b42
zahra-hsb <zahra16toa@gmail.com>
2024-10-29 05:59:08 -0700

    one problem fixed

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <Main />
       <Services />
-      <AboutComponent />
+      <AboutComponent isAboutUs={false} />
       <TitlesSection />
     </>
   );
