b69851eb004e8f8457d1a7107edb39da26410348
zahra-hsb <zahra16toa@gmail.com>
2024-11-03 05:03:22 -0800

    empty map component created and contact form and input component has edited

--- src/components/contactComponents/ContactForm/index.jsx
+++ src/components/contactComponents/ContactForm/index.jsx
@@ -1,14 +1,75 @@
 import Button from "@/components/globalComponents/Button"
 import Input from "@/components/globalComponents/Input"
+import { useState } from "react"
+import emailjs from 'emailjs-com';
 
 const ContactForm = () => {
+
+    const [values, setValues] = useState({
+        fullName: '',
+        email: '',
+        description: ''
+    })
+
+    function handleChange(e) {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        emailjs.send('service_bqh61l8', 'template_2oaefbt', values, 'jB241984fHUicJsXK')
+            .then(response => {
+                console.log('SUCCESS!')
+                setValues({
+                    fullName: '',
+                    email: '',
+                    description: ''
+                });
+                alert('پیام شما با موفقیت ارسال شد.')
+            }, error => {
+                alert('پیام ارسال نشد')
+            });
+    }
     return (
-        <form className="flex flex-col items-center gap-5 py-5 w-full">
-            <Input type={'text'} placeholder={'نام و نام خانوادگی'} />
-            <Input type={'email'} placeholder={'ایمیل'} />
-            <textarea placeholder="سوالات خود را در اینجا مطرح کنید..." className="w-full outline-none text-gray-600 border-b-2 border-b-green p-2" cols={20} rows={5} />
+        <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col items-center gap-5 py-5 w-full">
+            <Input
+                onChange={(e) => handleChange(e)}
+                required={true}
+                value={values.fullName}
+                type={'text'}
+                placeholder={'نام و نام خانوادگی'}
+                name={'fullName'}
+            />
+
+            <Input
+                onChange={(e) => handleChange(e)}
+                required={true}
+                value={values.email}
+                type={'email'}
+                placeholder={'ایمیل'}
+                name={'email'}
+            />
+
+            <textarea
+                onChange={(e) => handleChange(e)}
+                required={true}
+                value={values.description}
+                placeholder="سوالات خود را در اینجا مطرح کنید..."
+                className="w-full outline-none text-gray-600 border-b-2 border-b-green p-2"
+                cols={20}
+                rows={5}
+                name={'description'}
+            />
+
             <div className="flex items-end gap-2 w-full">
-                <Button onClick={() => handleClearForm()} link={'#'} style={'!py-2'}>ارسال پیام</Button>
+                <button type={'submit'} className={'!py-2 text-green text-nowrap flex min-w-24 bg-gradient-to-r from-darkGreen to-buttonOp px-5 sm:px-3 font-bold rounded-lg relative group overflow-hidden hover:text-white '}>
+                    <span className="absolute -translate-x-full opacity-0 group-hover:translate-x-full group-hover:opacity-100 top-0 right-0 cursor-pointer w-full h-full bg-gradient-to-r from-green to-green !duration-200 ease-linear"></span>
+
+                    ارسال پیام
+                </button>
                 <button type="reset" className={'pb-2 text-center border-2 px-3 py-1 border-green rounded-lg hover:text-white hover:bg-green transition-all duration-150'}>پاک کردن</button>
             </div>
         </form>
--- src/components/globalComponents/Input/index.jsx
+++ src/components/globalComponents/Input/index.jsx
@@ -1,7 +1,7 @@
-const Input = ({ type, placeholder, onKeyDown, style, value, onChange }) => {
+const Input = ({ type, placeholder, name, onKeyDown, style, value, onChange, required }) => {
     return (
         <>
-            <input type={type} placeholder={placeholder} onChange={onChange} value={value} onKeyDown={onKeyDown} className={`bg-transparent py-2 px-3 outline-none border-b-2 w-full border-b-green ${style}`} />
+            <input name={name} required={required} type={type} placeholder={placeholder} onChange={onChange} value={value} onKeyDown={onKeyDown} className={`bg-transparent py-2 px-3 outline-none border-b-2 w-full border-b-green ${style}`} />
         </>
     )
 }
