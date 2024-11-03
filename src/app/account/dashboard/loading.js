d8bc24d6ba35a5265454cc886f809b95bda846f4
zahra-hsb <zahra16toa@gmail.com>
2024-11-03 06:44:11 -0800

    loaing added in dashboard

---
+++ src/app/account/dashboard/loading.js
@@ -0,0 +1,11 @@
+import LoadingUI from "@/components/globalComponents/LoadingUI"
+
+const Loading = () => {
+    return (
+        <>
+            <LoadingUI />
+        </>
+    )
+}
+
+export default Loading
