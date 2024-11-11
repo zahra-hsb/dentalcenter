51b0db4016c659e5bbff4d096711da03a7598728
zahra-hsb <zahra16toa@gmail.com>
2024-11-11 08:58:40 -0800

    add admin endpoint created and added to add admin page

---
+++ src/app/api/addAdmin/route.js
@@ -0,0 +1,24 @@
+import dbConnect from "@/lib/dbconnect";
+import AdminsModel from "@/lib/models/AdminsModel";
+import { NextResponse } from "next/server";
+
+
+
+export async function POST(req) {
+    try {
+        await dbConnect()
+        const data = await req.json()
+
+        const adminIsExist = await AdminsModel.findOne({ tel: data.tel })
+        console.log(adminIsExist);
+        if (!adminIsExist) {
+            const admin = new AdminsModel(data)
+            admin.save()
+            return NextResponse.json({ message: 'ادمین شما با موفقیت اضافه شد.', isExist: false })
+        }
+        return NextResponse.json({ message: 'ادمین مورد نظر از قبل در سیستم ثبت شده است.', isExist: true })
+
+    } catch (error) {
+        return NextResponse.json({ err: error })
+    }
+}
