af8758b849d11382b4e1a4bf76282cdb5b4f8d90
zahra-hsb <zahra16toa@gmail.com>
2024-11-27 06:49:12 -0800

    url has changed

--- src/app/(main)/blogs/[slug]/page.js
+++ src/app/(main)/blogs/[slug]/page.js
@@ -12,7 +12,7 @@ export default Page
 
 
 export async function generateStaticParams() {
-    const posts = await fetch('http://localhost:3000/api/getBlogs').then((res) => res.json())
+    const posts = await fetch('http://drgomaryan.darkube.app/api/getBlogs').then((res) => res.json())
 
     return posts.map((post) => ({
         slug: post._id,
