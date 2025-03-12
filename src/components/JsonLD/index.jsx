f22ed9a8319a35afa2216f607d8732976cdd42ea
zahra-hsb <zahra16toa@gmail.com>
2025-03-12 12:24:01 -0700

    fix: update json ld

--- src/components/JsonLD/index.jsx
+++ src/components/JsonLD/index.jsx
@@ -1,57 +1,35 @@
 const JsonLD = () => {
-    const jsonld = {
-        "@context": "https://schema.org",
-        "@type": "Dentist",
-        name: 'Dr. Vahid Gomarian',
-        image: 'https://drgomaryan.ir/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.38468a3a.png&w=1080&q=75',
-        "@id": "",
-        url: "https://drgomaryan.ir",
-        address: [
-            {
+        const jsonld = {
+            "@context": "[https://schema.org](https://schema.org)",
+            "@type": "Dentist",
+            "@id": "[https://drgomaryan.ir/](https://drgomaryan.ir/)",
+            "name": "Dr. Vahid Gomarian",
+            "image": "[https://drgomaryan.ir/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.38468a3a.png&w=1080&q=75](https://drgomaryan.ir/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.38468a3a.png&w=1080&q=75)",
+            "url": "[https://drgomaryan.ir](https://drgomaryan.ir)",
+            "telephone": "02146807225",
+            "address": {
                 "@type": "PostalAddress",
                 "streetAddress": "Imam Khomeini blvd.",
                 "addressLocality": "Qods city",
                 "addressRegion": "Tehran",
-                "postalCode": "POSTCODE",
+                "postalCode": "3875637331", 
                 "addressCountry": "IR"
             },
-        ],
-        department: [
-            {
-                "@type": "Dentist",
-                "name": "Location",
-                "image": "",
-                "telephone": "02146807225",
-                "address": {
-                    "@type": "PostalAddress",
-                    "streetAddress": "Imam Khomeini blvd.",
-                    "addressLocality": "Qods city",
-                    "addressRegion": "Tehran",
-                    "postalCode": "POSTCODE",
-                    "addressCountry": "IR"
-                },
-                "openingHoursSpecification": {
+            "openingHoursSpecification": [
+                {
                     "@type": "OpeningHoursSpecification",
-                    "dayOfWeek": [
-                        "Saturday",
-                        "sunday",
-                        "Monday",
-                        "Tuesday",
-                        "Wednesday",
-                        "Thursday"
-                    ],
+                    "dayOfWeek": ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"],
                     "opens": "09:00",
                     "closes": "21:00"
                 }
-            },
-        ]
+            ],
+        }
+        return (
+            <>
+                <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonld) }}>
+                </script>
+            </>
+        )
     }
-    return (
-        <>
-            <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonld) }}>
-            </script>
-        </>
-    )
-}
 
-export default JsonLD
+    export default JsonLD
