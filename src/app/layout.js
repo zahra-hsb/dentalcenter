3d7c32550d0d06c972aaf7f2b005acdcfa2920a8
Zahra <zahra16toa@gmail.com>
2025-03-26 01:11:46 +0430

    fix: the main fav icon has added into project

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,21 +3,29 @@ import { Suspense } from "react";
 import Loading from "./loading";
 import { GlobalContextProvider } from "./context";
 import JsonLD from "@/components/JsonLD";
-
+import Head from "next/head";
 
 export const metadata = {
   title: "دنتال سنتر | دکتر وحید گماریان",
-  description: " ،کلینیک دندانپزشکی و زیبایی دکتر وحید گماریان - خدمات مطب: ایمپلنت اقساطی، روکش دندان، لمینت، ترمیم دندان ها، درمان ریشه، پروتز متحرک و دندانپزشکی اطفال در شهرقدس",
+  description:
+    " ،کلینیک دندانپزشکی و زیبایی دکتر وحید گماریان - خدمات مطب: ایمپلنت اقساطی، روکش دندان، لمینت، ترمیم دندان ها، درمان ریشه، پروتز متحرک و دندانپزشکی اطفال در شهرقدس",
+    // icons: {
+    //   icon: "/favicon.ico"
+    // }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased bg-bgColor max-w-screen-2xl mx-auto`}
-      >
+      <body className={`antialiased bg-bgColor max-w-screen-2xl mx-auto`}>
         <GlobalContextProvider>
-          <Suspense fallback={<><Loading /></>}>
+          <Suspense
+            fallback={
+              <>
+                <Loading />
+              </>
+            }
+          >
             {children}
           </Suspense>
         </GlobalContextProvider>
