6a109afda3f5b6ed2fd7e6045372d1fe06b2dee1
Zahra <zahra16toa@gmail.com>
2025-03-26 12:07:16 +0430

    fix: add validation id

--- src/app/api/getBlog/route.js
+++ src/app/api/getBlog/route.js
@@ -5,11 +5,19 @@ import BlogsModel from "@/lib/models/BlogsModel";
 
 export async function POST(req) {
     try {
-        const { id } = await req.json()
-        console.log('id: ', id);
+        const body = await req.json()
+        if (!body.id) {
+            return NextResponse.json({ error: "Missing ID" }, { status: 400 });
+        }
+        console.log('id: ', body.id);
         await dbConnect()
-        const res = await BlogsModel.findOne({ _id: new ObjectId(id) })
+        const res = await BlogsModel.findOne({ _id: new ObjectId(body.id) })
         console.log('result', res);
+
+        if (!res) {
+            return NextResponse.json({ error: "Blog not found" }, { status: 404 });
+        }
+        
         return NextResponse.json(res)
     } catch (error) {
         return NextResponse.json(error)
