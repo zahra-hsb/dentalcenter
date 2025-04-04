1cf6cfd7ce98451e5f81e3bb916c8e2f8219aa83
Zahra <zahra16toa@gmail.com>
2025-04-04 22:14:36 +0430

    style: add style to get better design

--- src/components/globalComponents/BlogCard/index.jsx
+++ src/components/globalComponents/BlogCard/index.jsx
@@ -1,34 +1,38 @@
-import Image from "next/image"
-import Container from "../Container"
-import Link from "next/link"
+import Image from "next/image";
+import Container from "../Container";
+import Link from "next/link";
 
 const BlogCard = ({ blogImg, title, author, summary, id }) => {
-    return (
-        <>
-            <Container style={'!items-start mb-10'}>
-                <Link href={`/blogs/${id}`}>
-                    <Image src={blogImg} width={200} height={200} alt="" />
-                </Link>
-                <Link href={`/blogs/${id}`}>
-                    <h4 className="group-hover:text-green">{title}</h4>
-                </Link>
-                <div>
-                    <div className="text-gray-600 text-sm group-hover:text-white" dangerouslySetInnerHTML={{ __html: summary }}>
-                    </div>
-                </div>
-                <div className="flex justify-between gap-5">
-                    <div className="flex gap-2 text-xs text-gray-500 text-nowrap group-hover:text-gray-300">
-                        <h5>نویسنده:</h5>
-                        <h5>{author}</h5>
-                    </div>
-                    {/* <div className="flex gap-2 text-xs text-gray-500 text-nowrap">
+  return (
+    <>
+      <Container style={"!items-start mb-10 !h-72 !justify-between"}>
+        <Link href={`/blogs/${id}`}>
+          <Image src={blogImg} width={200} height={200} alt="" className="rounded-lg max-h-36" />
+        </Link>
+        <Link href={`/blogs/${id}`}>
+          <h4 className="group-hover:text-green">{title}</h4>
+        </Link>
+        <div>
+          <div
+            className="text-gray-600 text-sm group-hover:text-white line-clamp-3"
+            dangerouslySetInnerHTML={{
+              __html: summary.length > 150 ? summary.split(0, 25) + "..." : summary,
+            }}
+          ></div>
+        </div>
+        <div className="flex justify-between gap-5">
+          <div className="flex gap-2 text-xs text-gray-500 text-nowrap group-hover:text-gray-300">
+            <h5>نویسنده:</h5>
+            <h5>{author}</h5>
+          </div>
+          {/* <div className="flex gap-2 text-xs text-gray-500 text-nowrap">
                         <h5>تاریخ بارگزاری:</h5>
                         <h5>{date}</h5>
                     </div> */}
-                </div>
-            </Container>
-        </>
-    )
-}
+        </div>
+      </Container>
+    </>
+  );
+};
 
-export default BlogCard
+export default BlogCard;
--- src/components/homeComponents/BlogsSlider/index.jsx
+++ src/components/homeComponents/BlogsSlider/index.jsx
@@ -1,72 +1,84 @@
-'use client'
-import axios from "axios"
-import { useEffect } from "react"
+"use client";
+import axios from "axios";
+import { useEffect } from "react";
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 
 // import required modules
-import { Autoplay, Pagination } from 'swiper/modules';
+import { Autoplay, Pagination } from "swiper/modules";
 import BlogCard from "@/components/globalComponents/BlogCard";
 
 const BlogSlider = () => {
-    const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
 
+  useEffect(() => {
+    async function getBlogs() {
+      try {
+        const res = await axios.get("/api/getBlogs");
+        setBlogs(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getBlogs();
+  }, []);
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center mb-10">
+        <h1 className="text-base font-bold text-green text-center">
+          پاسخ سوالات خود را می توانید از مقالات زیر پیدا کنید
+        </h1>
+        <h1 className="text-lg font-bold text-center">
+          آخرین مقالات
+        </h1>
+      </div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
+        autoplay={true}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        {blogs.length > 0 &&
+          blogs.map((item) => (
+            <SwiperSlide key={item._id}>
+              <BlogCard
+                author={item.author}
+                blogImg={item.blogImg}
+                date={item.date}
+                summary={item.summary}
+                title={item.title}
+                id={item._id}
+              />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </>
+  );
+};
 
-    useEffect(() => {
-        async function getBlogs() {
-            try {
-                const res = await axios.get('/api/getBlogs')
-                setBlogs(res.data)
-                console.log(res.data);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getBlogs()
-    }, [])
-    return (
-        <>
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={30}
-                pagination={{
-                    clickable: true,
-                }}
-                breakpoints={
-                    {
-                        640: {
-                            slidesPerView: 2,
-                            spaceBetween: 20,
-                        },
-                        768: {
-                            slidesPerView: 3,
-                            spaceBetween: 40,
-                        },
-                        1024: {
-                            slidesPerView: 4,
-                            spaceBetween: 50,
-                        },
-                    }
-                }
-                autoplay={true}
-                modules={[Pagination, Autoplay]}
-                className="mySwiper"
-            >
-                {blogs.length > 0 && blogs.map(item => (
-                    <SwiperSlide key={item._id}>
-                        <BlogCard author={item.author} blogImg={item.blogImg}
-                            date={item.date} summary={item.summary} title={item.title} id={item._id} />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </>
-    )
-}
-
-export default BlogSlider
+export default BlogSlider;
