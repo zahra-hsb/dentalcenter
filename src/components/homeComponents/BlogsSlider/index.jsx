f1b0c47856c08d3b1df66fe53edc66bd7a4cdd7a
zahra-hsb <zahra16toa@gmail.com>
2025-05-02 19:41:57 -0700

    fix: add condition to dont show if it does not exists

--- src/components/homeComponents/BlogsSlider/index.jsx
+++ src/components/homeComponents/BlogsSlider/index.jsx
@@ -22,7 +22,6 @@ const BlogSlider = () => {
       try {
         const res = await axios.get("/api/getBlogs");
         setBlogs(res.data);
-        console.log(res.data);
       } catch (error) {
         console.log(error);
       }
@@ -31,52 +30,52 @@ const BlogSlider = () => {
   }, []);
   return (
     <>
-      <div className="flex flex-col items-center justify-center mb-10">
-        <h1 className="text-base font-bold text-green text-center">
-          پاسخ سوالات خود را می توانید از مقالات زیر پیدا کنید
-        </h1>
-        <h1 className="text-lg font-bold text-center">
-          آخرین مقالات
-        </h1>
-      </div>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 50,
-          },
-        }}
-        autoplay={true}
-        modules={[Pagination, Autoplay]}
-        className="mySwiper"
-      >
-        {blogs.length > 0 &&
-          blogs.map((item) => (
-            <SwiperSlide key={item._id}>
-              <BlogCard
-                author={item.author}
-                blogImg={item.blogImg}
-                date={item.date}
-                summary={item.summary}
-                title={item.title}
-                id={item._id}
-              />
-            </SwiperSlide>
-          ))}
-      </Swiper>
+      {blogs.length > 0 &&
+      (
+      <><div className="flex flex-col items-center justify-center mb-10">
+          <h1 className="text-base font-bold text-green text-center">
+            پاسخ سوالات خود را می توانید از مقالات زیر پیدا کنید
+          </h1>
+          <h1 className="text-lg font-bold text-center">
+            آخرین مقالات
+          </h1>
+        </div><Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 50,
+            },
+          }}
+          autoplay={true}
+          modules={[Pagination, Autoplay]}
+          className="mySwiper"
+        >
+            {blogs.map((item) => (
+              <SwiperSlide key={item._id}>
+                <BlogCard
+                  author={item.author}
+                  blogImg={item.blogImg}
+                  date={item.date}
+                  summary={item.summary}
+                  title={item.title}
+                  id={item._id} />
+              </SwiperSlide>
+            ))}
+          </Swiper></>
+      )}
     </>
   );
 };
